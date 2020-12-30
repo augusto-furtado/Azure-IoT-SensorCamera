004befe58eb3955c10070c5b8efc00bc1a9147f7
augusto.furtado <augusto.furtado@accenture.com>
2020-12-30 09:03:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+SensorCamera
+This app take a picture using a camera (OmniVision OV5647 - 5MP) attached to RaspberryPI and send notification to IoT Hub.
+
+
+App architecture:
+Projects created from template ".Net Core Console - C#"
+App using Azure SDKs wich can connect to the Edge Device Gateway (being a downstream device) or can connect to the IoT Hub directly (DPS can be used to auto-provision).
---
+++ SensorCamera/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+namespace SensorCamera
+{
+    class Program
+    {
+        //DPS Scope ID
+        private static string idScope;
+        //keys from DPS enrollment group. 
+        private static string enrollmentGroupPrimaryKey;
+        private static string enrollmentGroupSecondaryKey;
+        //Registration Id for this Device - required if using DPS
+        private static string registrationId;
+        //Device String Connection - Use this or DPS info above to connect the device - add GatewayHostName at the end if a downstream device using a Edge Gateway
+        private static string deviceStringConnection;
+        //path to save picture taken
+        private static string pathToSavePicture;
+
+        private static DeviceClient deviceClient = null;
+        private static string[] config = null;
+
+        static void Main(string[] args)
+        {
+            //load app properties 
+            ReadConfigFile();
+            idScope = GetProperty("idScope");
+            enrollmentGroupPrimaryKey = GetProperty("enrollmentGroupPrimaryKey");
+            enrollmentGroupSecondaryKey = GetProperty("enrollmentGroupSecondaryKey");
+            registrationId = GetProperty("registrationId");
+            deviceStringConnection = GetProperty("deviceStringConnection");
+            pathToSavePicture = GetProperty("pathToSavePicture");
+
+            bool dpsInfoOk = !string.IsNullOrWhiteSpace(idScope) && !string.IsNullOrWhiteSpace(enrollmentGroupPrimaryKey) &&
+                 !string.IsNullOrWhiteSpace(enrollmentGroupSecondaryKey) && !string.IsNullOrWhiteSpace(registrationId);
+            bool directConnection = !string.IsNullOrWhiteSpace(deviceStringConnection);
+
+            if (!(dpsInfoOk || directConnection))
+            {
+                Console.WriteLine("ID Scope, Keys and Registration ID must be provided if using DPS, otherwise fill the Device Connection String");
+                Console.ReadLine();
+            }
+            else
+            {
+                //connect device directly if device string connection is known
+                if (!string.IsNullOrWhiteSpace(deviceStringConnection))
+                {
+                    //this install a root certificate in the OS - applicable if you using this device as a downstream to connect through a Edge Gateway. 
+                    if (deviceStringConnection.Contains("GatewayHostName"))
+                    {
+                        InstallCACert();
+                    }
+
+                    deviceClient = DeviceClient.CreateFromConnectionString(deviceStringConnection, TransportType.Mqtt_Tcp_Only);
+                }
+                else
+                {
+                    //Provision through DPS and return DeviceClient object
+                    ProvisioningDeviceClientWrapper provisionWrapper = new ProvisioningDeviceClientWrapper(
+                        idScope, registrationId, enrollmentGroupPrimaryKey, enrollmentGroupSecondaryKey);
+
+                    deviceClient = provisionWrapper.RunAsync().GetAwaiter().GetResult();
+                }
+                
+                // Create a handler for the direct method call
+                deviceClient.SetMethodHandlerAsync("TakePicture", TakePicture, null).Wait();
+
+                //open device connection
+                deviceClient.OpenAsync().ConfigureAwait(false);
+
+                //execute this method to program continue running
+                Console.ReadLine();
+
+                //close device connection
+                deviceClient.CloseAsync().ConfigureAwait(false);
+            }
+
+        }
+               
+        private static async void SendTelemetryData()
+        {
+            if (deviceClient != null)
+            {
+                string payload = "newImage";
+                Message message = new Message(Encoding.UTF8.GetBytes(payload));
+                await deviceClient.SendEventAsync(message).ConfigureAwait(false);
+
+                Console.WriteLine("Message sent from " + registrationId + " is: " + payload);
+            }
+        }
+
+        // Handle the direct method call - this method take a picture using the Camera and send notification to IoT Hub
+        private static Task<MethodResponse> TakePicture(MethodRequest methodRequest, object userContext)
+        {
+            try
+            {
+                var data = Encoding.UTF8.GetString(methodRequest.Data);
+
+                if (!string.IsNullOrWhiteSpace(data) && !(data == "null"))
+                {
+                    // Remove quotes from data, if any
+                    data = data.Replace("\"", "");
+
+                    pathToSavePicture = data;
+                    Console.WriteLine("new path to save picture is: " + pathToSavePicture);
+                }
+
+                //TODO take picture 
+                SendTelemetryData();
+
+                // Acknowledge the direct method call with a 200 success message.
+                string result = "{\"result\":\"Executed direct method: " + methodRequest.Name + "\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
+            }
+            catch (Exception exception)
+            {
+                // Acknowledge the direct method call with a 400 error message.
+                string result = "{\"result\":\"Exception: " + exception.ToString() + "\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 400));
+            }
+
+        }
+
+        /// <summary>
+        /// Add certificate in local cert store (at your OS) for use by downstream device
+        /// client for secure connection to IoT Edge runtime.
+        ///
+        ///    Note: On Windows machines, if you have not run this from an Administrator prompt,
+        ///    a prompt will likely come up to confirm the installation of the certificate.
+        ///    This usually happens the first time a certificate will be installed.
+        /// </summary>
+        static void InstallCACert()
+        {
+            string trustedCACertPath = "azure-iot-test-only.root.ca.cert.pem";
+            if (!string.IsNullOrWhiteSpace(trustedCACertPath))
+            {
+                Console.WriteLine("User configured CA certificate path: {0}", trustedCACertPath);
+                if (!File.Exists(trustedCACertPath))
+                {
+                    // cannot proceed further without a proper cert file
+                    Console.WriteLine("Certificate file not found: {0}", trustedCACertPath);
+                    throw new InvalidOperationException("Invalid certificate file.");
+                }
+                else
+                {
+                    Console.WriteLine("Attempting to install CA certificate: {0}", trustedCACertPath);
+                    X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Add(new X509Certificate2(X509Certificate.CreateFromCertFile(trustedCACertPath)));
+                    Console.WriteLine("Successfully added certificate: {0}", trustedCACertPath);
+                    store.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("trustedCACertPath was not set or null, not installing any CA certificate");
+            }
+        }
+
+        //The config file must have a property in each line and the key and value must be separated by " = "
+        static void ReadConfigFile()
+        {
+            // Read each line of the file into a string array. Each element of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"AppProperties.conf");
+            config = lines;
+        }
+
+        static string GetProperty(string property)
+        {
+            string separator = " = ";
+            foreach (string line in config)
+            {
+                if (property == line.Split(separator)[0])
+                    return line.Split(separator)[1];
+            }
+            return null;
+        }
+    }
+}
